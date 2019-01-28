2ecf13418695d47d98ea1c362529aa99ea6cac9b
djaso <djasokolov@gmail.com>
2019-01-28 20:34:28 +0300

    Order creation

---
+++ vseinet.ru/src/AppBundle/Bus/Order/Form/PassportDataType.php
@@ -0,0 +1,35 @@
+
+namespace AppBundle\Bus\Order\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use AppBundle\Bus\Order\Query\DTO\Passport;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class PassportType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('seria', TextType::class, ['required' => false,])
+            ->add('number', TextType::class, ['required' => false,])
+            ->add('issuedAt', DateType::class, [
+                'required' => false,
+                'widget' => 'single_text',
+                'html5' => false,
+                'format' => 'dd.MM.yyyy',
+                'input' => 'datetime',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Passport::class,
+        ]);
+    }
+}
