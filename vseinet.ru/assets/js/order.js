034f5b6323e3456bff2756f2e688a2ceb50e560a
djaso <djasokolov@gmail.com>
2019-01-30 18:25:36 +0300

    Order creation

--- vseinet.ru/assets/js/order.js
+++ vseinet.ru/assets/js/order.js
@@ -53,6 +53,13 @@ $(function() {
                 else{
                     $('body, html ').animate( { scrollTop: destination - 10}, 500 );
                 }
+            },
+            onSuccess: function(data) {
+                if (data.isInnerOrder) {
+                    window.location = Routing.generate('authority', { targetUrl: '/admin/orders/?id=' + data.id });
+                } else {
+                    window.location = Routing.generate('order_created_page', { id: data.id });
+                }
             }
         });
     }
