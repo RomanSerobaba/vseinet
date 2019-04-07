541b210397ce098228529d93781c9137e401c75a
djaso <djasokolov@gmail.com>
2019-04-08 00:54:37 +0300

    fix

--- vseinet.ru/src/AppBundle/Bus/Order/Query/DTO/OrganizationDetails.php
+++ vseinet.ru/src/AppBundle/Bus/Order/Query/DTO/OrganizationDetails.php
@@ -3,8 +3,7 @@
 namespace AppBundle\Bus\Order\Query\DTO;
 
 use Symfony\Component\Validator\Constraints as Assert;
-use AppBundle\Bus\User\Query\DTO\Contact;
-use AppBundle\Enum\OrderItemStatus;
+use AppBundle\Validator\Constraints as VIC;
 
 class OrganizationDetails
 {
@@ -19,26 +18,22 @@ class OrganizationDetails
     public $legalAddress;
 
     /**
-     * @Assert\Type(type="string")
-     * @Assert\Length(min=20, max=20, exactMessage="Номер расчетного счёта должен состоять из 9 цифр")
+     * @VIC\SettlementAccount
      */
     public $settlementAccount;
 
     /**
-     * @Assert\Type(type="string")
-     * @Assert\Length(min=12, max=12, exactMessage="ИНН должен состоять из 12 цифр")
+     * @VIC\TIN
      */
     public $tin;
 
     /**
-     * @Assert\Type(type="string")
-     * @Assert\Length(min=9, max=9, exactMessage="КПП должен состоять из 9 цифр")
+     * @VIC\KPP
      */
     public $kpp;
 
     /**
-     * @Assert\Type(type="string")
-     * @Assert\Length(min=9, max=9, exactMessage="БИК должен состоять из 9 цифр")
+     * @VIC\BIC
      */
     public $bic;
 
