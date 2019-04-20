6497a149149e10edfb10dc87e2c059c7ed905bec
djaso <djasokolov@gmail.com>
2019-04-20 18:42:46 +0300

    fix

--- vseinet.ru/src/AppBundle/Bus/Catalog/Query/GetSubcategoriesQueryHandler.php
+++ vseinet.ru/src/AppBundle/Bus/Catalog/Query/GetSubcategoriesQueryHandler.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Bus\Catalog\Query;
 
 use AppBundle\Bus\Message\MessageHandler;
+use AppBundle\Enum\ProductAvailabilityCode;
 
 class GetSubcategoriesQueryHandler extends MessageHandler
 {
@@ -40,15 +41,14 @@ class GetSubcategoriesQueryHandler extends MessageHandler
         foreach ($subcategories as $subcategory) {
             $q = $em->createQuery('
                 SELECT
-                    bpi.basename,
-                    RANDOM() HIDDEN ORD
+                    bpi.basename
                 FROM AppBundle:BaseProductImage bpi
                 INNER JOIN AppBundle:BaseProduct bp WITH bp.id = bpi.baseProductId
                 INNER JOIN AppBundle:CategoryPath cp WITH cp.id = bp.categoryId
-                WHERE cp.pid = :categoryId
-                ORDER BY ORD
+                WHERE cp.pid = :categoryId AND bp.supplierAvailabilityCode >= :productAvailabilityCode_AVAILABLE
             ');
             $q->setParameter('categoryId', $subcategory->id);
+            $q->setParameter('productAvailabilityCode_AVAILABLE', ProductAvailabilityCode::AVAILABLE);
             $q->setMaxResults(1);
             $image = $q->getOneOrNullResult();
             if (!empty($image)) {
