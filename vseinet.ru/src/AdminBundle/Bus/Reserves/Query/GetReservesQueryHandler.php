a9f7f4a9929ae7f9315a08f15121a0011e3389aa
djaso <djasokolov@gmail.com>
2019-06-07 17:15:21 +0300

    reserves fix

--- vseinet.ru/src/AdminBundle/Bus/Reserves/Query/GetReservesQueryHandler.php
+++ vseinet.ru/src/AdminBundle/Bus/Reserves/Query/GetReservesQueryHandler.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace AdminBundle\Bus\Reserves\Query;
 
@@ -7,6 +7,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use AppBundle\Entity\BaseProduct;
 use AppBundle\Enum\ProductAvailabilityCode;
 use AppBundle\Doctrine\ORM\Query\DTORSM;
+use AdminBundle\Bus\Reserves\Query\DTO\Supply;
+use AppBundle\Enum\GoodsConditionCode;
 
 class GetReservesQueryHandler extends MessageHandler
 {
@@ -15,55 +17,55 @@ class GetReservesQueryHandler extends MessageHandler
         $em = $this->getDoctrine()->getManager();
 
         $product = $em->getRepository(BaseProduct::class)->find($query->baseProductId);
+
         if (!$product instanceof BaseProduct) {
             throw new NotFoundHttpException(sprintf('Товар с кодом %d не найден', $query->baseProductId));
         }
 
         $q = $em->createNativeQuery("
-            SELECT 
+            SELECT
                 sd.did,
                 grrc.geo_room_id,
+                grrc.order_item_id,
                 grrc.destination_geo_room_id,
                 s.code,
                 sd.number,
                 sd.created_at,
                 grrc.delta,
                 si.purchase_price,
-                grrc.goods_condition_code
-            FROM goods_reserve_register_current AS grrc 
-            INNER JOIN supply_item AS si ON si.id = grrc.supply_item_id 
+                grrc.goods_condition_code,
+                grrc.goods_pallet_id
+            FROM goods_reserve_register_current AS grrc
+            INNER JOIN supply_item AS si ON si.id = grrc.supply_item_id
             INNER JOIN supply_doc AS sd ON sd.did = si.parent_did
-            INNER JOIN supplier AS s ON s.id = sd.supplier_id 
+            INNER JOIN supplier AS s ON s.id = sd.supplier_id
             WHERE grrc.base_product_id = :base_product_id
-        ", new DTORSM(DTO\Supply::class, DTORSM::ARRAY_ASSOC));
+        ", new DTORSM(DTO\Reserve::class, DTORSM::ARRAY_ASSOC));
         $q->setParameter('base_product_id', $product->getId());
-        $supplies = $q->getResult('DTOHydrator');
+        $reserves = $q->getResult('DTOHydrator');
 
-        if (empty($supplies)) {
+        if (empty($reserves)) {
             return new DTO\Reserves();
         }
 
         $roomIds = [];
-        foreach ($supplies as $supply) {
-            if (null !== $supply->geoRoomId) {
-                $roomIds[$supply->geoRoomId] = $supply->geoRoomId;
-            }
-            if (null !== $supply->destinationGeoRoomId) {
-                $roomIds[$supply->destinationGeoRoomId] = $supply->destinationGeoRoomId;
-            }
+
+        foreach ($reserves as $reserve) {
+            $geoRoomId = $reserve->geoRoomId ? $reserve->geoRoomId : $reserve->destinationGeoRoomId;
+            $roomIds[$geoRoomId] = $geoRoomId;
         }
 
         $q = $em->createNativeQuery("
-            SELECT 
+            SELECT
                 gr.id AS geo_room_id,
                 gr.name AS geo_room_name,
                 gp.id AS geo_point_id,
                 gp.name AS geo_point_name,
                 gc.id AS geo_city_id,
                 gc.name AS geo_city_name
-            FROM geo_room AS gr 
-            INNER JOIN geo_point AS gp ON gp.id = gr.geo_point_id 
-            INNER JOIN geo_city AS gc ON gc.id = gp.geo_city_id 
+            FROM geo_room AS gr
+            INNER JOIN geo_point AS gp ON gp.id = gr.geo_point_id
+            INNER JOIN geo_city AS gc ON gc.id = gp.geo_city_id
             WHERE gr.id IN (:ids)
         ", new DTORSM(DTO\GeoObject::class));
         $q->setParameter('ids', $roomIds);
@@ -72,29 +74,56 @@ class GetReservesQueryHandler extends MessageHandler
         $geoCities = [];
         $geoPoints = [];
         $geoRooms = [];
+
         foreach ($geoObjects as $geoObject) {
             if (empty($geoCities[$geoObject->geoCityId])) {
                 $geoCities[$geoObject->geoCityId] = new DTO\GeoCity($geoObject->geoCityId, $geoObject->geoCityName);
             }
+
             $geoCities[$geoObject->geoCityId]->geoPointIds[$geoObject->geoPointId] = $geoObject->geoPointId;
-            if (empty($geoPOints[$geoObject->geoPointId])) {
+
+            if (empty($geoPoints[$geoObject->geoPointId])) {
                 $geoPoints[$geoObject->geoPointId] = new DTO\GeoPoint($geoObject->geoPointId, $geoObject->geoPointName);
             }
+
             $geoPoints[$geoObject->geoPointId]->geoRoomIds[$geoObject->geoRoomId] = $geoObject->geoRoomId;
+
             if (empty($geoRooms[$geoObject->geoRoomId])) {
                 $geoRooms[$geoObject->geoRoomId] = new DTO\GeoRoom($geoObject->geoRoomId, $geoObject->geoRoomName);
             }
         }
 
-        foreach ($supplies AS $supply) {
-            if (null !== $supply->destinationGeoRoomId) {
-                $geoRooms[$supply->destinationGeoRoomId]->supplyIds[] = $supply->did;
+        foreach ($reserves as $reserve) {
+            $geoRoomId = $reserve->geoRoomId ? $reserve->geoRoomId : $reserve->destinationGeoRoomId;
+            if (!isset($geoRooms[$geoRoomId]->supplies[$reserve->supplyId])) {
+                $geoRooms[$geoRoomId]->supplies[$reserve->supplyId] = new Supply(
+                    $reserve->did,
+                    $reserve->number,
+                    $reserve->code,
+                    $reserve->createdAt,
+                    $reserve->purchasePrice
+                );
             }
-            elseif (null !== $supply->geoRoomId) {
-                $geoRooms[$supply->geoRoomId]->supplyIds[] = $supply->did;
+
+            if ($reserve->destinationGeoRoomId || $reserve->goodsPalletId) {
+                if (GoodsConditionCode::FREE === $reserve->goodsConditionCode) {
+                    $geoRooms[$geoRoomId]->supplies[$reserve->supplyId]->freeTransitDelta += $reserve->delta;
+                } elseif (GoodsConditionCode::RESERVED === $reserve->goodsConditionCode) {
+                    $geoRooms[$geoRoomId]->supplies[$reserve->supplyId]->reservedTransitDelta += $reserve->delta;
+                } else {
+                    $geoRooms[$geoRoomId]->supplies[$reserve->supplyId]->issuedTransitDelta += $reserve->delta;
+                }
+            } else {
+                if (GoodsConditionCode::FREE === $reserve->goodsConditionCode) {
+                    $geoRooms[$geoRoomId]->supplies[$reserve->supplyId]->freeDelta += $reserve->delta;
+                } elseif (GoodsConditionCode::RESERVED === $reserve->goodsConditionCode) {
+                    $geoRooms[$geoRoomId]->supplies[$reserve->supplyId]->reservedDelta += $reserve->delta;
+                } else {
+                    $geoRooms[$geoRoomId]->supplies[$reserve->supplyId]->issuedDelta += $reserve->delta;
+                }
             }
         }
 
-        return new DTO\Reserves($geoCities, $geoPoints, $geoRooms, $supplies);
+        return new DTO\Reserves($geoCities, $geoPoints, $geoRooms);
     }
 }
