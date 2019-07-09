69840fffb03fdb1bdd293a7fbe4c3c854697f52b
djaso <djasokolov@gmail.com>
2019-07-09 22:05:22 +0300

    fix

--- vseinet.ru/src/AppBundle/Session/NativeSessionStorage.php
+++ vseinet.ru/src/AppBundle/Session/NativeSessionStorage.php
@@ -22,7 +22,7 @@ class NativeSessionStorage extends BaseNativeSessionStorage
         session_id($id);
         session_start();
         session_decode($data);
-print_r($id);print_r(session_id());die();
+var_dump($id, session_id());die();
         $this->loadSession();
 
         setcookie($this->getName(), $id, 0, ini_get('session.cookie_path'), ini_get('session.cookie_domain'), ini_get('session.cookie_secure'), ini_get('session.cookie_httponly'));
