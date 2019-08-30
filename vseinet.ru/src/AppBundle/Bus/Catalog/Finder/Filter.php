1ac299ab50dadd5420162d7a8071e2559be498e8
djaso <djasokolov@gmail.com>
2019-08-30 20:47:07 +0300

    fix

--- vseinet.ru/src/AppBundle/Bus/Catalog/Finder/Filter.php
+++ vseinet.ru/src/AppBundle/Bus/Catalog/Finder/Filter.php
@@ -364,7 +364,7 @@ class Filter extends ContainerAware
 
     protected function parseStr(string $value): ?string
     {
-        $str = preg_replace('/\p{C}+/u', '', urldecode($value)) ?: null;
+        $str = preg_replace('/\p{C}+/u', '', urldecode(trim($value))) ?: null;
         if (empty($str) || 2 > mb_strlen($str)) {
             return null;
         }
