14563713b7e3350509c7e80d634a4b474338a3f2
djaso <djasokolov@gmail.com>
2019-11-22 11:48:38 +0300

    fix search

--- vseinet.ru/src/AppBundle/Bus/Catalog/Finder/AutocompleteFinder.php
+++ vseinet.ru/src/AppBundle/Bus/Catalog/Finder/AutocompleteFinder.php
@@ -104,7 +104,7 @@ class AutocompleteFinder extends AbstractProductFinder
             WHERE MATCH('{$expression}') AND availability <= {$availability}
             ORDER BY weight DESC, availability ASC, rating DESC, price ASC
             LIMIT ".self::COUNT_PRODUCTS."
-            OPTION ranker=expr('sum(if(sum_idf / 10 > 10, 10, sum_idf / 10) + if(min_best_span_pos < 5, 5, 0) + if(exact_order == 1, 5, 0)) + if(availability < 4, 4 - availability, 0) * 4 + if(is_accessories == 1, 0, 5) + if(category_average_price > 500000, 5, 0) + if(popularity > 50, 10, 0) + if(name_length < 150, 10, 0)')
+            OPTION ranker=expr('sum(if(sum_idf / 10 > 10, 10, sum_idf / 10) + if(min_best_span_pos < 5, 5, 0) + if(exact_order == 1, 5, 0)) + if(availability < 4, 4 - availability, 0) * 2 + if(is_accessories == 1, 0, 5) + if(category_average_price > 500000, 5, 0) + if(popularity > 50, 10, 0) + if(name_length < 150, 10, 0)')
             ;
         ";
         $results = $this->get('sphinx')->createQuery()->setQuery($query)->getResults();
