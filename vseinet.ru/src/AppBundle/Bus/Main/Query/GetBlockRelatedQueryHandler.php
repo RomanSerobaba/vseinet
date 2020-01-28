8b3108805b7690f98535867082ef874c6d70f899
djaso <djasokolov@gmail.com>
2020-01-28 19:48:01 +0300

    Изменение порядка вывода товаров в блоке С этим товаром покупают

--- vseinet.ru/src/AppBundle/Bus/Main/Query/GetBlockRelatedQueryHandler.php
+++ vseinet.ru/src/AppBundle/Bus/Main/Query/GetBlockRelatedQueryHandler.php
@@ -20,22 +20,28 @@ class GetBlockRelatedQueryHandler extends MessageHandler
                     bp.categoryId,
                     c.name,
                     COALESCE(p.price, p0.price),
-                    bpi.basename,
+                    FIRST(
+                        SELECT
+                            bpi.basename
+                        FROM AppBundle:BaseProductImage AS bpi
+                        WHERE bpi.baseProductId = bp.id AND bpi.sortOrder = 1 AND bpi.width > 0
+                    ),
                     bp.sefUrl
-                )
+                ),
+                MAX(o.createdAt) AS HIDDEN ORD,
+                COUNT(o.DId) AS HIDDEN ORD2
             FROM AppBundle:BaseProduct AS bp1
             INNER JOIN AppBundle:OrderItem AS oi WITH oi.baseProductId = bp1.id
             INNER JOIN AppBundle:OrderDoc AS o WITH o.DId = oi.orderDid
             INNER JOIN AppBundle:OrderItem AS oi2 WITH oi.orderDid = oi2.orderDid
             INNER JOIN AppBundle:BaseProduct AS bp2 WITH bp2.id = oi2.baseProductId
             INNER JOIN AppBundle:BaseProduct AS bp WITH bp.id = bp2.canonicalId
-            LEFT OUTER JOIN AppBundle:BaseProductImage AS bpi WITH bpi.baseProductId = bp.id AND bpi.sortOrder = 1 AND bpi.width > 0
             LEFT JOIN AppBundle:Product AS p WITH p.baseProductId = bp.canonicalId AND p.geoCityId = :geoCityId
             INNER JOIN AppBundle:Product AS p0 WITH p0.baseProductId = bp.canonicalId
-            INNER JOIN AppBundle:CategoryPath AS cp WITH cp.id = bp.categoryId
-            INNER JOIN AppBundle:Category AS c WITH c.id = cp.id
+            INNER JOIN AppBundle:Category AS c WITH c.id = bp.categoryId
             WHERE bp1.canonicalId = :id AND bp2.canonicalId != :id AND p0.geoCityId = 0 AND o.orderTypeCode IN (:orderTypeCodes) AND p0.productAvailabilityCode > :productAvailabilityCode_OUT_OF_STOCK
-            GROUP BY bp.id, c.id, p.price, p0.price, bpi.id
+            GROUP BY bp.id, c.id, p.price, p0.price
+            ORDER BY ORD2 DESC, ORD DESC
         ");
         $q->setParameter('id', $query->baseProductId);
         $q->setParameter('orderTypeCodes', [OrderTypeCode::SITE, OrderTypeCode::SHOP, OrderTypeCode::LEGAL, OrderTypeCode::REQUEST]);
