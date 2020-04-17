c37cda501387a82492b92875c1748c0e009395c5
djaso <djasokolov@gmail.com>
2020-04-17 15:19:31 +0300

    порядок категорий в бредкрамбсах при переносе товара

--- vseinet.ru/src/AdminBundle/Bus/Category/Query/SearchQueryHandler.php
+++ vseinet.ru/src/AdminBundle/Bus/Category/Query/SearchQueryHandler.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace AdminBundle\Bus\Category\Query;
 
@@ -11,22 +11,22 @@ class SearchQueryHandler extends MessageHandler
         $em = $this->getDoctrine()->getManager();
 
         $q = $em->createQuery("
-            SELECT 
+            SELECT
                 NEW AdminBundle\Bus\Category\Query\DTO\CategoryFound (
                     c.id,
                     c.name,
                     c.pid
                 ),
                 CASE WHEN c.name LIKE :qo THEN 1 ELSE 2 END AS HIDDEN ORD
-            FROM AppBundle:Category AS c 
-            WHERE c.aliasForId IS NULL 
+            FROM AppBundle:Category AS c
+            WHERE c.aliasForId IS NULL
                 AND (CASE WHEN EXISTS (
-                    SELECT 1 
-                    FROM AppBundle:Category AS cc 
+                    SELECT 1
+                    FROM AppBundle:Category AS cc
                     WHERE cc.pid = c.id
-                ) THEN false ELSE true END) = true 
-                AND (c.name LIKE :q1 OR c.name LIKE :q2) 
-            ORDER BY ORD, c.name  
+                ) THEN false ELSE true END) = true
+                AND (c.name LIKE :q1 OR c.name LIKE :q2)
+            ORDER BY ORD, c.name
         ");
         $q->setParameter('q1', $query->q.'%');
         $q->setParameter('q2', '%'.$query->q.'%');
@@ -35,20 +35,20 @@ class SearchQueryHandler extends MessageHandler
         $result = $q->getResult('IndexByHydrator');
         if (!empty($result)) {
             $q = $em->createQuery("
-                SELECT 
+                SELECT
                     NEW AdminBundle\Bus\Category\Query\DTO\CategoryFound (
                         c.id,
                         c.name,
-                        cp.id 
+                        cp.id
                     )
-                FROM AppBundle:Category AS c 
-                INNER JOIN AppBundle:CategoryPath AS cp WITH cp.pid = c.id 
+                FROM AppBundle:Category AS c
+                INNER JOIN AppBundle:CategoryPath AS cp WITH cp.pid = c.id
                 WHERE c.aliasForId IS NULL AND c.id > 0 AND cp.id IN (:ids)
-                ORDER BY cp.level 
+                ORDER BY cp.level
             ");
             $q->setParameter('ids', array_keys($result));
             foreach ($q->getResult() as $item) {
-                $items[$item->pid][$item->id] = $item->name;
+                $items[$item->pid][] = $item->name;
             }
             foreach ($items as $id => $path) {
                 $result[$id]->name = implode(' / ', $path);
