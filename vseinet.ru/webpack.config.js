bef64022b94db21c6c13779a90896cc271417b0a
djaso <djasokolov@gmail.com>
2020-10-12 16:52:56 +0300

    фикс ленивой загрузки изображений

--- vseinet.ru/webpack.config.js
+++ vseinet.ru/webpack.config.js
@@ -19,30 +19,18 @@ Encore
     })
     .addEntry('libs', './assets/libs.js')
     .addEntry('scripts', './assets/app.js')
-    .addStyleEntry('styles', './assets/app.css')
-    .enableVersioning(Encore.isProduction())
-    .enableSourceMaps(!Encore.isProduction());
-
-const es5Config = Encore.getWebpackConfig();
-
-Encore.reset();
-
-Encore
-    .setOutputPath('web/bundles/')
-    .setPublicPath('/bundles')
-    .cleanupOutputBeforeBuild()
     .configureBabel(function(babelConfig) {
         babelConfig.presets = [['env', {
             modules: false,
             useBuiltIns: true,
             targets: {
-            browsers: [
-                'Chrome >= 60',
-                'Safari >= 10.1',
-                'iOS >= 10.3',
-                'Firefox >= 54',
-                'Edge >= 15',
-            ],
+                browsers: [
+                    'Chrome >= 60',
+                    'Safari >= 10.1',
+                    'iOS >= 10.3',
+                    'Firefox >= 54',
+                    'Edge >= 15',
+                ],
             },
         }]];
     })
@@ -52,6 +40,4 @@ Encore
     .enableVersioning(Encore.isProduction())
     .enableSourceMaps(!Encore.isProduction());
 
-const es6Config = Encore.getWebpackConfig();
-
-module.exports = [es5Config, es6Config];
+module.exports = Encore.getWebpackConfig();
